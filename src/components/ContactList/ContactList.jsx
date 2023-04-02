7d1fc1696c92a849961d0aa7dfb8bd222167d089
SerhiiMukhin <110244388+SerhiiMukhin@users.noreply.github.com>
2023-04-02 10:25:09 +0300

    changed contacts filtering in ContactList

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,24 +2,23 @@ import { ContactListItem } from 'components/ContactListItem/ContactListItem';
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { getContacts, getFilter } from 'redux/selectors';
+import css from './ContactList.module.css';
 
 export const ContactList = () => {
   const contacts = useSelector(getContacts);
   const filter = useSelector(getFilter);
+  const filteredContacts = contacts.filter(({ name }) =>
+    name.toLowerCase().includes(filter)
+  );
 
   return (
     <ul>
-      {contacts.length > 0 ? (
-        contacts
-          .filter(({ name }) => name.toLowerCase().includes(filter))
-          .map(contact => (
-            <ContactListItem
-              key={contact.id}
-              contact={contact}
-            ></ContactListItem>
-          ))
+      {filteredContacts.length > 0 ? (
+        filteredContacts.map(contact => (
+          <ContactListItem key={contact.id} contact={contact}></ContactListItem>
+        ))
       ) : (
-        <p>No contacts added yet...</p>
+        <p className={css.message}>No contacts found...</p>
       )}
     </ul>
   );
